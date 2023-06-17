778c4450b5e9d2827a9906064cbb8d762d528927
TulioC5 <124647964+TulioC5@users.noreply.github.com>
2023-06-17 07:53:43 -0600

    ULTIMO

--- src/apis/tipo_queja.js
+++ src/apis/tipo_queja.js
@@ -41,6 +41,71 @@ router.get("/obtenerTipoQueja", async (req, res) =>{
     }        
 })
 
+
+router.get("/obtenerq", async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const payload = jwt.verify(token, secret);
+
+        // Obtener los parámetros de fecha desde la solicitud
+        const { desde, hasta } = req.query;
+
+        // Generar la consulta SQL con los filtros de fecha
+        let consulta = `SELECT queja.Siglas
+        ,queja.Nombres
+        ,queja.Apellidos
+        ,queja.Correo_email
+		,origen.Descripcion_Origen
+		,queja.Fecha_Queja 
+		,queja.Detalles
+		,qtipo.Descripcion as Descripcion3
+		,aten.Descripcion as Descripcion1
+		,regi.Descripcion as Descripcion2
+        ,estado.Descripcion FROM quejastulio.tbl_queja AS queja
+        JOIN quejastulio.tbl_estado AS estado ON queja.Id_EstadoIni = estado.Id_Estado
+        join quejastulio.tbl_queja_origen as origen on queja.Id_Origen 	= origen.Id_Origen 
+        join quejastulio.tbl_puntos_atencion as aten on queja.Id_PuntoA = aten.Id_PuntoA 
+        join quejastulio.tbl_regiones	     as regi on aten.Id_Region 	= regi.Id_Region
+        join quejastulio.tbl_tipo_queja		as qtipo on queja.Id_Tqueja = qtipo.Id_Tqueja`;
+        if (desde && hasta) {
+            consulta += ` WHERE queja.Fecha_Queja  >= '${desde}' AND queja.Fecha_Queja  <= '${hasta}'`;
+        }
+
+        console.log(consulta);
+        var resultadoConsulta = await realizarConsulta(consulta);
+        console.log(resultadoConsulta);
+        res.status(200).send({ resultadoConsulta });
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
+});
+
+module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 router.post("/actualizarTipoQueja", async (req, res) =>{
     try {
         const token = req.headers.authorization.split(" ")[1]
