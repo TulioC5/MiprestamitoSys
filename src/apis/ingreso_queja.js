f1792288a3c78cd8a3a87f5600b9ba80a7614565
TulioC5 <124647964+TulioC5@users.noreply.github.com>
2023-06-16 22:15:06 -0600

    makesssrzzxddd

--- src/apis/ingreso_queja.js
+++ src/apis/ingreso_queja.js
@@ -5,47 +5,34 @@ const jwt = require("jsonwebtoken");
 const secret = "123456";
 const tabla = 'quejastulio.tbl_queja';
 const multer = require('multer');
-const fs = require('fs');
-var { conexion, realizarConsulta, realizarDml } = require('../db/conexion');
-var {transporter, enviarCorreo} = require('../db/correos')
-const storage = multer.diskStorage({
-    destination: 'C:/Archivos', //crear
-   filename: function(req, file, cb) {
-    const ext = path.extname(file.originalname);
-    const newName = "Recibido" + ext; // Cambiar 'nuevo_nombre' por el nombre deseado
-    cb(null, newName);
-  }
-  });
-  const upload = multer({ storage: storage });
+const { conexion, realizarConsulta, realizarDml } = require('../db/conexion');
+const { transporter, enviarCorreo } = require('../db/correos');
 
-  router.post("/ingresarQueja", upload.any(), async (req, res) => {
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+router.post("/ingresarQueja", upload.single('File'), async (req, res) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
         const payload = jwt.verify(token, secret);
         const { Nombres, Apellidos, Correo_email, Telefono, Detalle, Id_TQueja, Id_Origen } = req.body;
-        var consulta = `select * from quejastulio.tbl_tipo_queja where Id_Tqueja = ${Id_TQueja}`;
-        var tiposQueja = await realizarConsulta(consulta);
+
+        const tiposQueja = await realizarConsulta(`SELECT * FROM quejastulio.tbl_tipo_queja WHERE Id_Tqueja = ${Id_TQueja}`);
         const currentDate = new Date();
         const day = String(currentDate.getDate()).padStart(2, '0');
         const month = String(currentDate.getMonth() + 1).padStart(2, '0');
         const year = currentDate.getFullYear();
-        var siglas = tiposQueja[0]['Siglas'] + "-" + tiposQueja[0]['Correlativo'] + "-" + year;
-        console.log(siglas);
-        const archivoAdjunto = req.files[0]; // Información del archivo adjunto
-        const nuevaCarpeta = 'C:/Archivos/Quejas/'; // Ruta de la nueva carpeta
-        var extension = path.extname(archivoAdjunto.originalname);
-        const nombreArchivo = siglas + "-" + day + "-" + month + "-" + year + extension; // Obtener el nombre del archivo
-        const rutaArchivoAnterior = archivoAdjunto.path; // Obtener la ruta del archivo temporal
-        const nuevaRutaArchivo = nuevaCarpeta + nombreArchivo; // Construir la nueva ruta del archivo
-        fs.renameSync(rutaArchivoAnterior, nuevaRutaArchivo); // Mover el archivo a la nueva ruta
-        console.log(rutaArchivoAnterior);
-        consulta = `INSERT INTO ${tabla}(Nombres, Apellidos, Correo_email, Telefono, Detalles, Id_TQueja, Id_Origen, Documento_Archivo, Id_PuntoA, Id_EstadoIni, Id_EstadoFin, Id_Usuario, Siglas) 
-        VALUES ('${Nombres}','${Apellidos}', '${Correo_email}', '${Telefono}', '${Detalle}', '${Id_TQueja}', '${Id_Origen}', '${nuevaRutaArchivo}', 1,1,1, '${payload.user}', '${siglas}')`;
-        console.log(consulta);
-        var resultadoConsulta = await realizarDml(consulta);
-        console.log(resultadoConsulta);
-        if (resultadoConsulta == true) {
-            //Enviar correo de confirmación
+        const siglas = tiposQueja[0]['Siglas'] + "-" + tiposQueja[0]['Correlativo'] + "-" + year;
+
+        const archivoAdjunto = req.file; // Información del archivo adjunto
+
+        const consulta = `INSERT INTO ${tabla}(Nombres, Apellidos, Correo_email, Telefono, Detalles, Id_TQueja, Id_Origen, Documento_Archivo, Id_PuntoA, Id_EstadoIni, Id_EstadoFin, Id_Usuario, Siglas) 
+        VALUES ('${Nombres}','${Apellidos}', '${Correo_email}', '${Telefono}', '${Detalle}', '${Id_TQueja}', '${Id_Origen}', '${archivoAdjunto.buffer.toString('base64')}', 1,1,1, '${payload.user}', '${siglas}')`;
+
+        const resultadoConsulta = await realizarDml(consulta);
+
+        if (resultadoConsulta === true) {
+            // Enviar correo de confirmación
             enviarCorreo('miprestamito@gmail.com', Correo_email, 'Queja ingresada correctamente', `Señor cuentahabiente,  agradecemos su comunicación,  le informamos que su queja ha sido recibida exitosamente. Para el seguimiento o cualquier consulta relacionada, no olvide que el número de su queja es ${siglas} `)
                 .then((enviado) => {
                     if (enviado) {
@@ -84,12 +71,9 @@ const storage = multer.diskStorage({
                     });
             }
 
-            consulta = `update quejastulio.tbl_tipo_queja set Correlativo = ${parseInt(tiposQueja[0]['Correlativo'] + 1)} where Id_Tqueja = ${Id_TQueja}`;
-            await realizarDml(consulta);
-            consulta = `insert into quejastulio.tbl_bitacora_db(Tbl_Nombre, Accion, Registro_Despues, Usuario, Fecha) VALUES ('${tabla}', '${"insertar"}',
-            'Descripcion: ${Detalle}, Nombres: ${Nombres} + ${Apellidos}', '${payload.user}', ${"Current_Timestamp"})`;
-            console.log(consulta);
-            await realizarDml(consulta);
+            await realizarDml(`UPDATE quejastulio.tbl_tipo_queja SET Correlativo = ${parseInt(tiposQueja[0]['Correlativo'] + 1)} WHERE Id_Tqueja = ${Id_TQueja}`);
+            await realizarDml(`INSERT INTO quejastulio.tbl_bitacora_db(Tbl_Nombre, Accion, Registro_Despues, Usuario, Fecha) VALUES ('${tabla}', 'insertar', 'Descripcion: ${Detalle}, Nombres: ${Nombres} + ${Apellidos}', '${payload.user}', CURRENT_TIMESTAMP)`);
+
             res.status(201).send({ Ok: "Ok" });
         } else {
             res.status(400).send({ Error: "Solicitud no válida" });
@@ -99,70 +83,52 @@ const storage = multer.diskStorage({
     }
 });
 
-
-
-
-
-router.get("/obtenerTipoQueja", async (req, res) =>{
+router.get("/obtenerTipoQueja", async (req, res) => {
     try {
-        const token = req.headers.authorization.split(" ")[1]
-        const payload = jwt.verify(token, secret)        
-        var consulta = `SELECT * FROM ${tabla}`;
-        console.log(consulta);
-        var resultadoConsulta = await realizarConsulta(consulta);
-        console.log(resultadoConsulta);                 
-        res.status(201).send({ resultadoConsulta });            
-    }catch (error) {
-        res.status(401).send({error: error.message})
-    }        
-})
+        const token = req.headers.authorization.split(" ")[1];
+        const payload = jwt.verify(token, secret);
+        const consulta = `SELECT * FROM ${tabla}`;
+        const resultadoConsulta = await realizarConsulta(consulta);
+        res.status(200).send({ resultadoConsulta });
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
+});
 
-router.post("/actualizarTipoQueja", async (req, res) =>{
+router.post("/actualizarTipoQueja", async (req, res) => {
     try {
-        const token = req.headers.authorization.split(" ")[1]
-        const payload = jwt.verify(token, secret)        
-        const {id: sub,Id_TQueja, Descripcion, Siglas } = req.body;
-        var consulta = `UPDATE ${tabla} set Descripcion = '${Descripcion}', Siglas = '${Siglas}' WHERE Id_Tqueja = ${Id_TQueja}`;        
-        console.log(consulta);
-        var resultadoConsulta = await realizarDml(consulta);
-        console.log(resultadoConsulta);
-        if (resultadoConsulta == true){    
-            consulta = `insert into quejastulio.tbl_bitacora_db(Tbl_Nombre, Accion, Registro_Despues, Usuario, Fecha) VALUES ('${tabla}', '${"actualizar"}',
-            'Id: ${Id_TQueja},Descripcion: ${Descripcion}, Siglas:${Siglas}', '${payload.user}', ${"Current_Timestamp"})`;
-            console.log(consulta);
-            var resultadoConsulta = await realizarDml(consulta);            
+        const token = req.headers.authorization.split(" ")[1];
+        const payload = jwt.verify(token, secret);
+        const { id: sub, Id_TQueja, Descripcion, Siglas } = req.body;
+        const consulta = `UPDATE ${tabla} SET Descripcion = '${Descripcion}', Siglas = '${Siglas}' WHERE Id_Tqueja = ${Id_TQueja}`;
+        const resultadoConsulta = await realizarDml(consulta);
+        if (resultadoConsulta === true) {
+            await realizarDml(`INSERT INTO quejastulio.tbl_bitacora_db(Tbl_Nombre, Accion, Registro_Despues, Usuario, Fecha) VALUES ('${tabla}', 'actualizar', 'Id: ${Id_TQueja}, Descripcion: ${Descripcion}, Siglas:${Siglas}', '${payload.user}', CURRENT_TIMESTAMP)`);
             res.status(200).send({ Ok: "Ok" });
         } else {
-            res.status(400).send({Error: "Solicitud no válida"})
-        }           
-    }catch (error) {
-        res.status(401).send({error: error.message})
-    }        
-})
+            res.status(400).send({ Error: "Solicitud no válida" });
+        }
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
+});
 
-router.post("/eliminarTipoQueja", async (req, res) =>{
+router.post("/eliminarTipoQueja", async (req, res) => {
     try {
-        const token = req.headers.authorization.split(" ")[1]
-        const payload = jwt.verify(token, secret)        
-        const {id: sub,Id_TQueja} = req.body;
-        var consulta = `UPDATE ${tabla} set Estado = 2 WHERE Id_Tqueja = ${Id_TQueja}`;        
-        console.log(consulta);
-        var resultadoConsulta = await realizarDml(consulta);
-        console.log(resultadoConsulta);
-        if (resultadoConsulta == true){    
-            consulta = `insert into quejastulio.tbl_bitacora_db(Tbl_Nombre, Accion, Registro_Despues, Usuario, Fecha) VALUES ('${tabla}', '${"eliminar"}', 'Id: ${Id_TQueja}, Estado: 2', '${payload.user}', ${"Current_Timestamp"})`;
-            console.log(consulta);
-            var resultadoConsulta = await realizarDml(consulta);            
+        const token = req.headers.authorization.split(" ")[1];
+        const payload = jwt.verify(token, secret);
+        const { id: sub, Id_TQueja } = req.body;
+        const consulta = `UPDATE ${tabla} SET Estado = 2 WHERE Id_Tqueja = ${Id_TQueja}`;
+        const resultadoConsulta = await realizarDml(consulta);
+        if (resultadoConsulta === true) {
+            await realizarDml(`INSERT INTO quejastulio.tbl_bitacora_db(Tbl_Nombre, Accion, Registro_Despues, Usuario, Fecha) VALUES ('${tabla}', 'eliminar', 'Id: ${Id_TQueja}, Estado: 2', '${payload.user}', CURRENT_TIMESTAMP)`);
             res.status(200).send({ Ok: "Ok" });
         } else {
-            res.status(400).send({Error: "Solicitud no válida"})
-        }           
-    }catch (error) {
-        res.status(401).send({error: error.message})
-    }        
-})
-
-
-
+            res.status(400).send({ Error: "Solicitud no válida" });
+        }
+    } catch (error) {
+        res.status(401).send({ error: error.message });
+    }
+});
 
 module.exports = router;
